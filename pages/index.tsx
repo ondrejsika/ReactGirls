4864bcc6a1e7241cbf0e53f731e305feb8d364c7
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-23 13:28:36 +0100

    Udate index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
 export default () => {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <h1>React Girls Prague</h1>
+      <p>
+        Anarcho-capitalism is a political philosophy and economic theory that
+        advocates the elimination of centralized state domination in favor of
+        self-ownership, private property and free markets.
+      </p>
+    </div>
+  );
 };
