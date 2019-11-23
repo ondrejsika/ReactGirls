0ce91c623f70efbd2cc559b5f7d561351e2fbbc3
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-23 16:03:24 +0100

    Add color props to Hello componet

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from "react";
 
 type HelloProps = {
-  name?: string;
+  name: string;
+  color?: string;
 };
-const Hello: FunctionComponent<HelloProps> = ({ name }) => (
-  <div style={{ color: "green" }}>
+
+const Hello: FunctionComponent<HelloProps> = ({ name, color }) => (
+  <div style={{ color: color || "green" }}>
     <h1>Hello {name}</h1>
   </div>
 );
